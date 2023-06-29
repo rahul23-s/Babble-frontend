8bcdd8f6189cc81ffa5e5094d5234f74dfb8ee4f
Rahul <rahul23shrm@gmail.com>
2023-06-29 18:35:45 +0530

    Logs fix

--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import store from "./store";
 import { backendEndpoint } from "./constants";
 
 axios.defaults.baseURL = backendEndpoint;
-
 const app = createApp(App);
 
 app.config.globalProperties.$axios = axios;
 app.use(router);
 app.use(store);
+window.console.log = ()=>{};
 app.mount("#app");
